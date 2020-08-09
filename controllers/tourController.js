2a8e3175a5c76a4ef189c664ecc02afbdff0d14e
olegs7771 <olegs7771@gmail.com>
2020-08-09 16:00:02 +0300

    ok

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -20,30 +20,41 @@ const checkNewTour = (req, res, next) => {
 };
 
 //Get All Tours
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
+const getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
 
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'Error',
+      message: err,
+    });
+  }
 };
 //Get Tour
-const getTour = (req, res) => {
-  console.log('req.params', req.params);
-  // const tour = tours.find((elem) => elem.id === req.params.id * 1);
-
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-
-    // data: {
-    //   tour,
-    // },
-  });
+const getTour = async (req, res) => {
+  try {
+    console.log('req.params', req.params);
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      response: 'Error',
+      message: err,
+    });
+  }
 };
 //Add New Tour
 const addNewTour = async (req, res) => {
@@ -64,12 +75,24 @@ const addNewTour = async (req, res) => {
   }
 };
 //Update File Data
-const updateTour = (req, res) => {
-  console.log('req', req);
-  res.status(200).json({
-    result: 'success',
-    tour: 'Updated',
-  });
+const updateTour = async (req, res) => {
+  try {
+    let tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.status(201).json({
+      status: 'Success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      result: 'Error',
+      message: err,
+    });
+  }
 };
 //Delete File Data
 const deleteTour = (req, res) => {
