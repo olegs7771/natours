54cf09f2509c2fae3522c4c89326cd9caf894934
olegs7771 <olegs7771@gmail.com>
2020-08-17 16:31:37 +0300

    🚀

--- app.js
+++ app.js
@@ -32,11 +32,5 @@ app.all('*', (req, res, next) => {
     message: `Can't find ${req.originalUrl} on this server!`,
   });
 });
-app.all('*/:', (req, res, next) => {
-  res.status(404).json({
-    status: 'Fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
-});
 
 module.exports = app;
