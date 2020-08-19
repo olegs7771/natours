ea7838ae68a15556a90562555e3d5b97d844a38d
Oleg Smushkevich <olegs777@bezeqint.net>
2020-08-19 19:55:45 +0300

    ok

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const tours = require('./routes/tours');
 const users = require('./routes/users');
+const errorControl = require('./controllers/errorController');
 
 const app = express();
 
@@ -38,13 +39,6 @@ app.all('*', (req, res, next) => {
 });
 
 //Error Handling in Express (app)
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(errorControl);
 
 module.exports = app;
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,32 +1,40 @@
-const sendErrorDev = (err,res)=>{
- res.status(err.statusCode).json({
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+const sendErrorProd = (err, res) => {
+  //Operational,trusted error:send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
       status: err.status,
-      error: err,
       message: err.message,
-      stack: err.stack,
     });
-    }
-    const sendErrorDev=(err,res)=>{
+    //Programming or other error: dodn't leak error details
+  } else {
+    //1) Log Error
+    console.error('ERROR ❗️', err);
+    //2) Send generic message
 
-    }
-
-
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went very wrong',
+    });
+  }
+};
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err,res)
-  }else if(process.env.NODE_ENV==='production'{
-  sendErrorDev(err,res)
-      
-    });
-    
-  })
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    // stack: err.stack,
-  });
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  } else {
+    console.log('test');
+  }
 };
