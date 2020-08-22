7c6654656cc2e5d79c469b0abdc36f398ba401ea
Oleg Smushkevich <olegs777@bezeqint.net>
2020-08-22 18:57:17 +0300

    ok

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,27 @@
 const User = require('../models/User');
 const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 const signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  console.log('req.body', req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXP,
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: newUser,
   });
 });
 
-module.exports = { signup };
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+};
+
+module.exports = { signup, login };
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
@@ -39,6 +40,15 @@ const userSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
+//Middleware
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  //Incrypt password
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
