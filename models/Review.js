e7afbde1ad7e7ffd23c7755ad446669a819105e2
olegs7771 <olegs7771@gmail.com>
2020-09-10 14:07:33 +0300

    ok

--- models/Review.js
+++ models/Review.js
@@ -1,5 +1,8 @@
 //revie /rating/createAt/ ref to tour/ref to user
+//pre uses next()
+//post NO next()!!
 const mongoose = require('mongoose');
+const Tour = require('../models/Tour');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -47,14 +50,15 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
-//Static function
+//Static function for calculating in Tour
 reviewSchema.statics.calcAverageRatings = async function (tourId) {
   const stats = await this.aggregate([
     {
-      $match: { tour: tourId },
+      $match: { tour: tourId }, //finds tour by id
     },
     {
       $group: {
+        //calculates average of all reviews
         _id: 'tour',
         nRating: { $sum: 1 },
         avgRating: { $avg: '$rating' },
@@ -62,11 +66,42 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
     },
   ]);
   console.log('stats', stats);
+  //Persisit stats in Tour DB
+  await Tour.findByIdAndUpdate(
+    tourId,
+    {
+      //updates tour with calculated data
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    },
+    {
+      new: true,
+    }
+  );
 };
 reviewSchema.post('save', function () {
+  //calling static function
   //this.constructor-->points to Model
   //this -->points to document to be saved
   this.constructor.calcAverageRatings(this.tour);
+});
+
+//findByIdAndUpdate findByIdAndDelete ==> query Middleware. mongoDB  uses findOneAndUpdate,findOneAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  //save to this to move from pre to post document
+  this.r = await this.findOne();
+  console.log('this.r pre', this.r);
+  this.time = Date.now();
+
+  next();
+});
+//passing this to post
+reviewSchema.post(/^findOneAnd/, async function () {
+  //await this.findOne(); does NOT work here! query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
+  console.log('this.r post', this.r);
+  console.log('time', Date.now() - this.time);
+  console.log();
 });
 
 //Allow nested routes
