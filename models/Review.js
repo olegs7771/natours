ee0b0d85ed613ca3351ca0e581821027e89aeb42
Oleg Smushkevich <olegs777@bezeqint.net>
2020-09-18 09:32:12 +0300

    ok

--- models/Review.js
+++ models/Review.js
@@ -44,7 +44,7 @@ reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
-    select: 'name ',
+    select: 'name photo',
   })
     // .populate({
     //   path: 'user',
