6e6350bf4fd36d0198708494d62eee3e39829f75
Oleg Smushkevich <olegs777@bezeqint.net>
2020-09-19 14:07:31 +0300

    ok

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -9,4 +9,32 @@ var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/olegs777/ckf9c8omb0if119rwgysy93wg',
   interactive: false,
+  // center: [-73.5804, 45.53483],
+  // pitch: 0,
+  // bearing: -60,
+  // zoom: 10,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+locations.map((loc) => {
+  //Create Marker
+  const elem = document.createElement('div');
+  elem.className = 'marker';
+  //Add Marker
+  new mapboxgl.Marker({
+    element: elem,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+  //Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 200,
+    left: 100,
+    right: 100,
+  },
 });
