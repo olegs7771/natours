47779aee1112ea10f91595731698c04666fdec60
Oleg Smushkevich <olegs777@bezeqint.net>
2020-10-05 17:00:48 +0300

    ok

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,1 +1,24 @@
-console.log(Stripe(process.env.STRIPE_SECRET_KEY));
+/*eslint-disable*/
+const stripe = Stripe(
+  'pk_test_51HYQowAwETtguWEQkKgdyt1IZgugTb4uPkJCxyDnbdzezk3Ei7WehhHwc74nrEmMgLiWSb9KZcYOIwGXbko763Hx00m3F7KcZt'
+);
+
+const bookBtn = document.getElementById('book-tour');
+
+const bookTour = async (tourId) => {
+  // 1) Get checkout session from API
+  try {
+    const session = await axios.get(
+      `http://127.0.0.1:8000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log('session', session);
+    // 2) Create checkout form + charge credit card
+  } catch (err) {
+    console.log('error', err);
+  }
+};
+
+bookBtn.addEventListener('click', (e) => {
+  console.log('e', e.target.dataset.tourId);
+  bookTour(e.target.dataset.tourId);
+});
