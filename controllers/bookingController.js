3d73f1dadcd0e5efefca1e9fd8b6f142053ec1a7
Oleg Smushkevich <olegs777@bezeqint.net>
2020-10-10 12:26:09 +0300

    ok

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -66,7 +66,7 @@ const webhookCheckout = (req, res, next) => {
   let event;
   try {
     event = stripe.webhooks.constructEvent(
-      req.rawBody,
+      req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
     );
