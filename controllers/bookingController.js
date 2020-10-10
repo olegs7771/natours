1f771b9bc365f05b5f9ac46040e7a7a65ab043a3
Oleg Smushkevich <olegs777@bezeqint.net>
2020-10-10 15:15:28 +0300

    ok

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -48,10 +48,11 @@ const getCheckOutSession = catchAsync(async (req, res, next) => {
 });
 
 const createBookingCheckout = async (session) => {
-  const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
-  await Booking.create({ tour, user, price });
+  res.status(200).json({ session });
+  // const tour = session.client_reference_id;
+  // const user = (await User.findOne({ email: session.customer_email })).id;
+  // const price = session.line_items[0].amount / 100;
+  // await Booking.create({ tour, user, price });
 };
 
 const webhookCheckout = (req, res, next) => {
