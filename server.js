022c25093644a286d369ce09664fcb85ed8a1117
olegs7771 <olegs7771@gmail.com>
2020-11-22 17:20:59 +0200

    ok

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 //Handle Uncaught Exeptions (errors,bugs in asynchrones code)
 process.on('uncaughtException', (err) => {
-  // console.log(err.name, ':', err.message);
-  // console.log('UNCAUGHT EXEPTION 💥 Shutting Down..');
+  console.log(err.name, ':', err.message);
+  console.log('UNCAUGHT EXEPTION 💥 Shutting Down..');
   process.exit(1);
 });
 const app = require('./app');
